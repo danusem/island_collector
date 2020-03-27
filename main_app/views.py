cc0a661541f4b24a6ab672b96d84b55a94721840
Dan Usem <danusem@gmail.com>
2020-03-26 22:56:43 -0500

    updated about view

--- main_app/views.py
+++ main_app/views.py
@@ -6,4 +6,4 @@ def home(request):
     return HttpResponse('<h1>Its an Island Ting</h1>')
 
 def about(request):
-    return HttpResponse('<h1>About the Island Collector</h1>')
+    return render(request, 'about.html')
