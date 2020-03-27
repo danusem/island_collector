743cad7e9443f4756323ac733ede2a99e1ab2c47
Dan Usem <danusem@gmail.com>
2020-03-26 22:41:07 -0500

    define about view function

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpResponse
 
+# Create your views here.
 def home(request):
     return HttpResponse('<h1>Its an Island Ting</h1>')
+
+def about(request):
+    return HttpResponse('<h1>About the Island Collector</h1>')
