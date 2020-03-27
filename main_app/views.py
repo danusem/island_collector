fb51938b2d4a826ae2cf30bd7e9d64ee79922e8a
Dan Usem <danusem@gmail.com>
2020-03-26 22:00:46 -0500

    update home view

--- main_app/views.py
+++ main_app/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+    return HttpResponse('<h1>Its an Island Ting</h1>')
