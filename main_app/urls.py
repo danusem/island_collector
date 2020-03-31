ed0d7161b285b2abc2493a78a1e35a1a855adf09
Dan Usem <danusem@gmail.com>
2020-03-31 16:08:02 -0500

    update forecast and save functionality

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('islands/', views.islands_index, name='index'),
     path('islands/<int:island_id>/', views.islands_detail, name='detail'),
+    path('islands/<int:island_id>/add_forecast/', views.add_forecast, name='add_forecast'),
     path('islands/create/', views.IslandCreate.as_view(), name='islands_create'),
     path('islands/<int:pk>/update/', views.IslandUpdate.as_view(), name='islands_update'),
     path('islands/<int:pk>/delete/', views.IslandDelete.as_view(), name='islands_delete'),
--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 from .models import Island
@@ -26,6 +26,16 @@ def islands_detail(request, island_id):
             'weather_form': weather_form
         }
     )
+
+def add_forecast(request, island_id):
+    form = WeatherForm(request.POST)
+
+    if form.is_valid():
+        new_forecast = form.save(commit=False)
+        new_forecast.island_id = island_id
+        new_forecast.save()
+
+    return redirect('detail', island_id=island_id)
 
 class IslandCreate(CreateView):
     model = Island
