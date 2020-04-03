3d78e398091fa464803685754bcdec0988003a5d
Dan Usem <danusem@gmail.com>
2020-04-03 13:48:31 -0500

    add characteristics url paths

--- main_app/urls.py
+++ main_app/urls.py
@@ -10,4 +10,9 @@ urlpatterns = [
     path('islands/create/', views.IslandCreate.as_view(), name='islands_create'),
     path('islands/<int:pk>/update/', views.IslandUpdate.as_view(), name='islands_update'),
     path('islands/<int:pk>/delete/', views.IslandDelete.as_view(), name='islands_delete'),
+    path('characteristics/<int:pk>/', views.CharacteristicDetail.as_view(), name='characteristics_detail'),
+    path('characteristics/<int:pk>/update/', views.CharacteristicUpdate.as_view(), name='characteristics_update'),
+    path('characteristics/<int:pk>/delete/', views.CharacteristicDelete.as_view(), name='characteristics_delete'),
+    path('characteristics/create/', views.characteristicCreate.as_view(), name='characteristics_create'),
+    path('cats/<int:cat_id>/assoc_characteristic/<int:characteristic_id>/', views.assoc_characteristic, name='assoc_characteristic'),
 ]
