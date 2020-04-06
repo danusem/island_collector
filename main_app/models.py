a762d449f107e1bcb439a3f226746d7baa06c35d
Dan Usem <danusem@gmail.com>
2020-04-06 18:26:17 -0500

    added Photo model

--- main_app/models.py
+++ main_app/models.py
@@ -43,3 +43,10 @@ class Weather(models.Model):
 
     class Meta:
         ordering = ['-date']
+
+class Photo(models.Model):
+    url = models.CharField(max_length=200)
+    island = models.ForeignKey(Island, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Photo for island_id: {self.island_id} @{self.url}"
