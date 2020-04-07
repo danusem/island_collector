c952711d3a2dc6a74d75a85352ae6b5a0952e204
Dan Usem <danusem@gmail.com>
2020-04-06 19:29:08 -0500

    update Photo in admin

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Island, Weather
+from .models import Island, Weather, Photo
 
 # Register your models here
 
 admin.site.register(Island)
 admin.site.register(Weather)
+admin.site.register(Photo)
